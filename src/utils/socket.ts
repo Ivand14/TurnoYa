612c197665e430ce8316a9faaa72ec9b1176aa20
Ivand14 <carpivan14@hotmail.com>
2025-06-22 23:09:05 -0300

    fix: Refactor socket connection options for improved configuration

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://turnosya-backend.onrender.com",
-    {
-        transports:["websocket"]
-    }
-)
+export const socket = io("https://turnosya-backend.onrender.com", {
+  transports: ["websocket"],
+  upgrade: false,
+  withCredentials: true,
+  reconnection: false, // Opcional si querés evitar que reintente con otro transporte
+});
