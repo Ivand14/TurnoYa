1ec479518771cedcdb5e42bd9da259c48e9498cd
Ivand14 <carpivan14@hotmail.com>
2025-06-23 00:04:36 -0300

    fix: Add WebSocket connection and event handlers for Gevent

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -7,3 +7,10 @@ export const socket = io("https://turnosya-backend.onrender.com", {
   reconnection: false
 });
 
+const ws = new WebSocket("wss://turnosya-backend.onrender.com/socket.io/?EIO=4&transport=websocket");
+ws.onopen = () => console.log("🟢 WebSocket activo con Gevent");
+ws.onerror = (e) => console.error("❌ Falla WebSocket con Gevent:", e);
+
+const newweb = new WebSocket("wss://turnosya-backend.onrender.com/socket.io/?EIO=4&transport=websocket")
+
+console.log(newweb);
