da1b25821d38f4b1956390be76b8873e0707f401
Ivand14 <carpivan14@hotmail.com>
2025-06-23 15:12:00 -0300

    fix: Update WebSocket reconnection setting and remove unused WebSocket instances

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -4,13 +4,5 @@ export const socket = io("https://turnosya-backend.onrender.com", {
   transports: ["websocket"],
   upgrade: false,
   withCredentials: true,
-  reconnection: false
+  reconnection: true
 });
-
-const ws = new WebSocket("wss://turnosya-backend.onrender.com/socket.io/?EIO=4&transport=websocket");
-ws.onopen = () => console.log("🟢 WebSocket activo con Gevent");
-ws.onerror = (e) => console.error("❌ Falla WebSocket con Gevent:", e);
-
-const newweb = new WebSocket("wss://turnosya-backend.onrender.com/socket.io/?EIO=4&transport=websocket")
-
-console.log(newweb);
