5c722f0838a8062e103109e75971bfae9b570359
Ivand14 <carpivan14@hotmail.com>
2025-08-05 14:37:37 -0300

    feat: add goToCalendar function to navigate to calendar on mobile; update Calendar and ServiceCard components to utilize new functionality

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -2,13 +2,20 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { Button } from "@/components/ui/button";
 import { Service } from "@/types";
+import { useRef } from "react";
 
 interface ServiceCardProps {
   service: Service;
   onReserve?: (serviceId: string) => void;
+  goToCalendar: (ref: React.RefObject<HTMLDivElement>) => void;
 }
 
-export const ServiceCard = ({ service, onReserve }: ServiceCardProps) => {
+export const ServiceCard = ({
+  service,
+  onReserve,
+  goToCalendar,
+}: ServiceCardProps) => {
+  const calendarRef = useRef<HTMLDivElement>(null);
 
   return (
     <Card className="overflow-hidden">
@@ -16,18 +23,36 @@ export const ServiceCard = ({ service, onReserve }: ServiceCardProps) => {
         <div>
           <CardTitle className="text-lg">{service.name_service}</CardTitle>
           <div className="flex items-center mt-1 text-sm text-gray-500">
-            <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <svg
+              className="w-4 h-4 mr-1"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
             </svg>
             {service.duration} minutos
           </div>
         </div>
-        <div className="text-xl font-bold text-booking-primary">${service.price}</div>
+        <div className="text-xl font-bold text-booking-primary">
+          ${service.price}
+        </div>
       </CardHeader>
       <CardContent className="p-4 pt-2">
         <p className="text-sm text-gray-600 mb-3">{service.description}</p>
-        <Button 
-          onClick={() => onReserve?.(service.id)} 
+        <Button
+          onClick={() => {
+            onReserve?.(service.id);
+            if (window.innerWidth <= 768) {
+              goToCalendar(calendarRef); // solo se ejecuta en mobile o tablets
+            }
+          }}
           className="w-full"
         >
           Reservar
